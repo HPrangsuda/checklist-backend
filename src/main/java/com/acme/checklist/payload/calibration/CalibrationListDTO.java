9b51409a5b15fc4ae8b66bd66276c78482f261e0
prangsuda94 <prangsuda.k@acme-inter.com>
2026-06-19 11:08:27 +0700

    add kanban

--- src/main/java/com/acme/checklist/payload/calibration/CalibrationListDTO.java
+++ src/main/java/com/acme/checklist/payload/calibration/CalibrationListDTO.java
@@ -13,30 +13,28 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalibrationListDTO {
-    private Long id;
-    private String machineCode;
-    private String machineName;
-    private Integer years;
+    private Long      id;
+    private String    machineCode;
+    private String    machineName;
+    private String    years;
     private LocalDate dueDate;
     private LocalDate startDate;
     private LocalDate certificateDate;
-    private String results;
-    private String calibrationStatus;
+    private String    results;
+    private String    calibrationStatus;
 
-    public static CalibrationListDTO from(CalibrationRecord calibrationRecord) {
-        if (calibrationRecord == null) {
-            return null;
-        }
+    public static CalibrationListDTO from(CalibrationRecord r) {
+        if (r == null) return null;
         return CalibrationListDTO.builder()
-                .id(calibrationRecord.getId())
-                .machineCode(calibrationRecord.getMachineCode())
-                .machineName(calibrationRecord.getMachineName())
-                .years(Integer.valueOf(calibrationRecord.getYears()))
-                .dueDate(calibrationRecord.getDueDate())
-                .startDate(calibrationRecord.getStartDate())
-                .certificateDate(calibrationRecord.getCertificateDate())
-                .results(calibrationRecord.getResults())
-                .calibrationStatus(calibrationRecord.getCalibrationStatus())
+                .id(r.getId())
+                .machineCode(r.getMachineCode())
+                .machineName(r.getMachineName())
+                .years(r.getYears())
+                .dueDate(r.getDueDate())
+                .startDate(r.getStartDate())
+                .certificateDate(r.getCertificateDate())
+                .results(r.getResults())
+                .calibrationStatus(r.getCalibrationStatus())
                 .build();
     }
 }
